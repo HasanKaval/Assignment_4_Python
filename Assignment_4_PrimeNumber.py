0abfff672b1b33f95d133018ecfe840d774ba6e4
HasanKaval <hasankaval77@gmail.com>
2020-07-15 01:22:40 +0200

    Update Assignment_4_PrimeNumber.py
    Made some PEP -8 corrections

--- Assignment_4_PrimeNumber.py
+++ Assignment_4_PrimeNumber.py
@@ -1,8 +1,8 @@
 number = int(input("Please enter a number  : "))
 counter = 0
 for i in range(1, number + 1):
-  if number%i == 0:
-    counter +=1
+  if number % i == 0:
+    counter += 1
 if counter > 2 or number == 0 or number == 1:
   print(number, "is not a prime number")
 else:
